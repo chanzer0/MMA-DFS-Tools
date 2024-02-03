142d2c35ad7300e286cc41c996a8a41d5bd6cd45
sean <seansailer28@gmail.com>
2024-02-03 13:11:51 -0600

    template

---
+++ src/mma_optimizer.py
@@ -0,0 +1,11 @@
+class MMA_Optimizer:
+    def __init__(self, site, num_lineups, num_uniques):
+        self.site = site
+        self.num_lineups = num_lineups
+        self.num_uniques = num_uniques
+
+    def optimize(self):
+        pass
+
+    def output(self):
+        pass
